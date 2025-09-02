1ab6d70490b8565edc82f58811c64d7a36fd9bda
Kyle Manayon <kylemanayon.bposeats@gmail.com>
2025-09-02 14:38:55 +0800

    add frontend base files

---
+++ product-list-fe/src/components/jsx/Sidebar.jsx
@@ -0,0 +1,14 @@
+function Sidebar() {
+    return (
+        <>
+            <h2>Sidebar</h2>
+            <ul>
+                <li>Item 1</li>
+                <li>Item 2</li>
+                <li>Item 3</li>
+            </ul>
+        </>
+    )
+}
+
+export default Sidebar;
---
+++ product-list-fe/src/components/jsx/Table.jsx
@@ -0,0 +1,14 @@
+function Table() {
+    return (
+        <>
+            <h2>Sidebar</h2>
+            <ul>
+                <li>Item 1</li>
+                <li>Item 2</li>
+                <li>Item 3</li>
+            </ul>
+        </>
+    )
+}
+
+export default Table;
