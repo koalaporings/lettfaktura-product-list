3cf9326eeeb8f608115dab84b33b83b301991222
Kyle <kalan.manayon@gmail.com>
2025-09-03 23:08:58 +0800

    change baseURL

--- product-list-fe/src/store/api.js
+++ product-list-fe/src/store/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "http://https://lettfaktura-product-list.onrender.com:8000",
   headers: {
     "Content-Type": "application/json",
   },
